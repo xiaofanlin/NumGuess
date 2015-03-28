266db076a1d6c401abbb0f6ef4df1b057740b911
xiaofanlin <lxf123@sjtu.edu.cn>
2015-03-28 18:57:04 +0800

    The first homework

--- 作业.cpp
+++ 作业.cpp
@@ -1,3 +1,4 @@
+//Filename NumGuess
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
@@ -14,17 +15,18 @@ int main(){
     {
         srand(time(NULL));
         int key[3] = {rand() * 10 / (RAND_MAX + 1), rand() * 10 / (RAND_MAX + 1), rand() * 10 / (RAND_MAX + 1)};
-        if ((key[0]==key[1])||(key[0]==key[2])||(key[2]==key[1])) continue; //make sure the three numbers are different
+        if ((key[0] == key[1])||(key[0] == key[2])||(key[2] == key[1])) continue;
+        //make sure the three numbers are different
 
         cout<<"需要玩游戏吗（Y/N）?"<<endl;
         cin>>choice_cin;
-        if (choice_cin.size()!=1) continue;
+        if (choice_cin.size() != 1) continue;
         choice = toupper(choice_cin[0]);
-        if ((choice!='Y')&&(choice!='N')) continue; //make sure the choice can only be {y,n,Y,N}
+        if ((choice != 'Y')&&(choice != 'N')) continue; //make sure the choice can only be {y,n,Y,N}
 
         if (choice == 'Y')
         {
-            for (counter = 0;counter < 7; ++counter)
+            for (counter = 0; counter < 7; ++counter)
             {
                 A = 0;
                 B = 0;
@@ -32,12 +34,13 @@ int main(){
                 while (1)
                 {
                     cout<<"请输入你猜测的数字："<<endl;
-                    cin>> answer_cin;
+                    cin>>answer_cin;
                     answer1 = answer_cin[0];
                     answer2 = answer_cin[1];
                     answer3 = answer_cin[2];
-                    if ((answer_cin.size()==3)&&(answer1 >= '0')&&(answer1 <= '9')&&
-                        (answer2 >= '0')&&(answer2 <= '9')&&(answer3 >= '0')&&(answer3 <= '9')) //make sure the input can only be three numbers
+                    if ((answer_cin.size() == 3)&&(answer1 >= '0')&&(answer1 <= '9')&&
+                        (answer2 >= '0')&&(answer2 <= '9')&&(answer3 >= '0')&&(answer3 <= '9'))
+                        //make sure the input can only be three numbers
                         break;
                 }
                 int answer[3] = {answer1 - '0', answer2 - '0', answer3 - '0'}; //change it form char to int
@@ -46,12 +49,12 @@ int main(){
                 {
                     for (i = 0; i < 3; ++i)
                     {
-                        if ((i==j)&&(key[i]==answer[j])) ++A;
+                        if ((i == j)&&(key[i] == answer[j])) ++A;
                         else
-                            if (key[i]==answer[j]) ++B; //compare answer and key to get A B
+                            if (key[i] == answer[j]) ++B; //compare answer and key to get A B
                     }
                 }
-                if (A==3)
+                if (A == 3)
                 {
                     ++win;
                     cout<<"恭喜，你猜对了"<<endl;
@@ -60,7 +63,7 @@ int main(){
                 else cout<<A<<"A"<<B<<"B"<<endl;
             }
 
-            if (counter==7)
+            if (counter == 7)
             {
                 ++loss;
                 cout<<"很遗憾，你没有在规定次数内猜对。答案是 ";
@@ -71,7 +74,7 @@ int main(){
         }
         else
         {
-            cout<<"你一共玩了"<<win+loss<<"局，赢了"<<win<<"局，输了"<<loss<<"局";
+            cout<<"你一共玩了"<<win + loss<<"局，赢了"<<win<<"局，输了"<<loss<<"局";
             break;
         }
     }
